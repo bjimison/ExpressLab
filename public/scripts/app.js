055e67959dc6469d5438eacda65ee10758eb3b33
bjimison <brady.t.jimison@gmail.com>
2018-07-11 09:18:32 -0700

    express lab

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+
 console.log("Sanity Check: JS is working!");
 
 const handleSuccess = json =>
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
   const express = require('express');
   const app = express();
+
   const bootStrap = require('bootstrap');
 
   // $('body').alert('This alert came from Bootstrap');
